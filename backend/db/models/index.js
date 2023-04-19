58785203af4cfc9144f54e749a656243703a34d6
Helen Koo <helenkoo2017@gmail.com>
2023-04-18 17:02:06 -0700

    Add User authentication Express middleware

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,47 +1,48 @@
 "use strict";
-const { Model, Validator } = require("sequelize");
 
-module.exports = (sequelize, DataTypes) => {
- class User extends Model {
-  static associate(models) {
-   // define association here
-  }
- }
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const process = require("process");
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../../config/database.js")[env];
+const db = {};
 
- User.init(
-  {
-   username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-     len: [4, 30],
-     isNotEmail(value) {
-      if (Validator.isEmail(value)) {
-       throw new Error("Cannot be an email.");
-      }
-     },
-    },
-   },
-   email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-     len: [3, 256],
-     isEmail: true,
-    },
-   },
-   hashedPassword: {
-    type: DataTypes.STRING.BINARY,
-    allowNull: false,
-    validate: {
-     len: [60, 60],
-    },
-   },
-  },
-  {
-   sequelize,
-   modelName: "User",
-  }
+let sequelize;
+if (config.use_env_variable) {
+ sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+ sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
  );
- return User;
-};
+}
+
+fs
+ .readdirSync(__dirname)
+ .filter((file) => {
+  return (
+   file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  );
+ })
+ .forEach((file) => {
+  const model = require(path.join(__dirname, file))(
+   sequelize,
+   Sequelize.DataTypes
+  );
+  db[model.name] = model;
+ });
+
+Object.keys(db).forEach((modelName) => {
+ if (db[modelName].associate) {
+  db[modelName].associate(db);
+ }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
