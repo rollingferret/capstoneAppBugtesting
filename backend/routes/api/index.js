4159f211ca9cc2776526ae236a70570ca2776264
Helen Koo <helenkoo2017@gmail.com>
2023-04-18 17:27:47 -0700

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,16 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
 // backend/routes/api/index.js
 // GET /api/restore-user
 const { restoreUser } = require("../../utils/auth.js");
 
-// backend/routes/api/index.js
-// ...
+// GET /api/set-token-cookie
+const { setTokenCookie } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
+// GET /api/require-auth
+const { requireAuth } = require("../../utils/auth.js");
 
 router.post("/test", function (req, res) {
  res.json({ requestBody: req.body });
@@ -13,9 +18,6 @@ router.post("/test", function (req, res) {
 // backend/routes/api/index.js
 // ...
 
-// GET /api/set-token-cookie
-const { setTokenCookie } = require("../../utils/auth.js");
-const { User } = require("../../db/models");
 router.get("/set-token-cookie", async (_req, res) => {
  const user = await User.findOne({
   where: {
@@ -28,13 +30,18 @@ router.get("/set-token-cookie", async (_req, res) => {
 });
 router.use(restoreUser);
 
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
+
+router.post("/test", (req, res) => {
+ res.json({ requestBody: req.body });
+});
 //
 router.get("/restore-user", (req, res) => {
  return res.json(req.user);
 });
 
-// GET /api/require-auth
-const { requireAuth } = require("../../utils/auth.js");
 router.get("/require-auth", requireAuth, (req, res) => {
  return res.json(req.user);
 });
