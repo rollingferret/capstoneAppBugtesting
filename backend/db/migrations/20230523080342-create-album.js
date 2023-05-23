b2e2e7ef0af7c908e74db7310eceb2f370aa4d4c
Helen Koo <helenkoo2017@gmail.com>
2023-05-23 10:27:27 -0700

    add album migration  and model files

---
+++ backend/db/migrations/20230523080342-create-album.js
@@ -0,0 +1,51 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === "production") {
+ options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
+module.exports = {
+ async up(queryInterface, Sequelize) {
+  await queryInterface.createTable(
+   "Albums",
+   {
+    id: {
+     allowNull: false,
+     autoIncrement: true,
+     primaryKey: true,
+     type: Sequelize.INTEGER,
+    },
+    name: {
+     allowNull: false,
+     type: Sequelize.STRING,
+    },
+    category: {
+     allowNull: false,
+     type: Sequelize.STRING,
+    },
+    ownerId: {
+     type: Sequelize.INTEGER,
+     references: {
+      model: "Users",
+     },
+    },
+    createdAt: {
+     allowNull: false,
+     type: Sequelize.DATE,
+     defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updatedAt: {
+     allowNull: false,
+     type: Sequelize.DATE,
+     defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+   },
+   options
+  );
+ },
+ async down(queryInterface, Sequelize) {
+  options.tableName = "Albums";
+  await queryInterface.dropTable(options);
+ },
+};
